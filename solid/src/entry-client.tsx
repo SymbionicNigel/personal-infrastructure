9d7cb8727d955090e77a92e267ab89a626e9bdf1
Nigel <nigel@gritseed.com>
2022-11-25 23:35:33 -0500

    chore: add lint changes

--- solid/src/entry-client.tsx
+++ solid/src/entry-client.tsx
@@ -1,4 +1,4 @@
-import { mount, StartClient } from "solid-start/entry-client";
+import { mount, StartClient } from 'solid-start/entry-client';
 import 'reflect-metadata';
 
 mount(() => <StartClient />, document);
