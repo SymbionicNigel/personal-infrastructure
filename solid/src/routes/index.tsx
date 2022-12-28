6a3a43cf936275d731c4d47494449bbb87d808a8
Nigel <nigel@gritseed.com>
2022-12-27 21:40:04 -0500

    feat: initial header

--- solid/src/routes/index.tsx
+++ solid/src/routes/index.tsx
@@ -1,7 +1,9 @@
-import { Button } from '@suid/material';
-import { refetchRouteData, useRouteData } from 'solid-start';
-import { createServerAction$ } from 'solid-start/server';
-import { logout } from '~/db/session';
+// import { Button } from '@suid/material';
+// import { refetchRouteData, useRouteData } from 'solid-start';
+// import { createServerAction$ } from 'solid-start/server';
+// import { logout } from '~/db/session';
+import MenuIcon from '@suid/icons-material/Menu';
+import { AppBar, Button, IconButton, Stack, Toolbar, Typography } from '@suid/material';
 import { useUser } from '../db/useUser';
 
 export function routeData() {
@@ -9,22 +11,39 @@ export function routeData() {
 }
 
 export default function Home() {
-	const user = useRouteData<typeof routeData>();
-	const [, { Form }] = createServerAction$((f: FormData, { request }) =>
-		logout(request)
-	);
+	// const user = useRouteData<typeof routeData>();
+	// const [, { Form }] = createServerAction$((f: FormData, { request }) =>
+	// 	logout(request)
+	// );
 
 	return (
-		<main class="w-full p-4 space-y-2">
-			<h1 class="font-bold text-3xl">Hello {user()?.username}</h1>
-			<h3 class="font-bold text-xl">Message board</h3>
-			<button onClick={() => refetchRouteData()}>Refresh</button>
-			<Button variant="contained">Hello world</Button>
-			<Form>
-				<button name="logout" type="submit">
-          Logout
-				</button>
-			</Form>
+		<main>
+			<AppBar position='static'>
+				<Stack
+					direction="row"
+					justifyContent="space-between"
+					alignItems="center"
+					spacing={4}
+				>
+					<IconButton
+						color='inherit'
+						aria-label="menu"
+						sx={{width: '2.5rem'}}
+					>
+						<MenuIcon/>
+					</IconButton>
+					<Typography variant="h6" component="div">
+						Symbionic 
+					</Typography>
+					<Button
+						sx={{ width: 'auto' }}
+						type='button'
+						size='small'
+						color="inherit">
+						Login
+					</Button>
+				</Stack>
+			</AppBar>
 		</main>
 	);
 }
