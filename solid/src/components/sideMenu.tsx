1f9ee09c688ad8d0e612e1c19ff0340590c03fcf
Nigel <nigel@gritseed.com>
2023-03-21 00:19:21 -0400

    feat: add side menu links and functionality

--- solid/src/components/sideMenu.tsx
+++ solid/src/components/sideMenu.tsx
@@ -1,20 +1,63 @@
-import { Stack } from '@suid/material';
+import { IconButton, Link, Stack, useTheme } from '@suid/material';
 import Fab from '@suid/material/Fab';
+import { Close, ReceiptLong, Widgets } from '@suid/icons-material';
+import { createSignal, Show } from 'solid-js';
+import { useLocation, useNavigate } from 'solid-start';
 
 export default function SideMenu() {
+	const navigate = useNavigate();
+	const location = useLocation();
+	const [expanded, setExpanded] = createSignal(false);
+	const theme = useTheme();
+
+	const clickExpanded = () => {
+		setExpanded(!expanded());
+	};
+
+	const handleMenuClick = (destination: string) => {
+		if (destination !== location.pathname) {
+			navigate(destination);
+			clickExpanded();
+		}
+	};
+
 	return (
 		<Stack
 			alignContent='flex-start'
 			position='fixed'
 			bottom='2rem'
 			left='2rem'
-			gap='1rem'
+			gap='0.5rem'
 			zIndex='1000'
 		>
-			<Fab variant='extended' color='primary' />
-			<Fab variant='extended' color='secondary' />
-			<Fab variant='extended' color='success' />
-			<Fab variant='extended' color='warning' />
+			<Show
+				when={expanded()}
+				fallback={
+					<Fab variant='extended' color='secondary' onclick={clickExpanded}>
+						<Widgets />
+					</Fab>
+				}
+			>
+				<Link
+					underline='hover'
+					variant='h3'
+					color={theme.palette.secondary.dark}
+					onClick={() => handleMenuClick('/resume')}
+				>
+					<ReceiptLong fontSize='large' />
+					Résumé
+					<Show when={location.pathname === '/resume'}>&middot</Show>
+				</Link>
+				<IconButton
+					sx={{ width: 'fit-content' }}
+					color='secondary'
+					onClick={clickExpanded}
+					type='button'
+					size='large'
+				>
+					<Close />
+				</IconButton>
+			</Show>
 		</Stack>
 	);
 }
